0abada2e7829e7f8015d75aa6387b97dde360499
Guri Saini <gurisaini@Guris-MacBook-Air.local>
2021-06-02 10:43:23 -0700

    add routes in main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
-import 'package:app/home_page.dart';
+import 'package:app/pages/home_page.dart';
+import 'package:app/pages/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,7 +11,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      theme: ThemeData(
+          primarySwatch: Colors.deepPurple,
+          fontFamily: GoogleFonts.lato().fontFamily),
+      themeMode: ThemeMode.light,
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      routes: {
+        "/login": (context) => HomePage(),
+        '/': (context) => LoginPage()
+      },
     );
   }
 }
