6fe5f2d627a6cad6f20d0a103d546b6e0b2ba15c
Guri Saini <gurisaini@Guris-MacBook-Air.local>
2021-06-02 10:42:15 -0700

    add pages

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    int days = 130;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("App Testing"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Hello ${days}"),
+        ),
+      ),
+     
+    );
+  }
+
+}
